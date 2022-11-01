bb17b38b946ed0eea0d840a225af1f23098b16ed
Marc-Olivier-Rancoeur <marc-olivier.rancoeur@isen-ouest.yncrea.fr>
2022-11-01 12:27:29 -0400

    MergeStructure added

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <pthread.h>
 
+#define THREADS 2
+
+struct MergeStruct{
+    int* tab;
+    int start;
+    int end;
+    int profondeur;
+};
+
 void* ThdMergeSort(void* pointer){
 }
 
-int* MergeSort(int* tab, int start, int end){
+int* MergeSort(int* tab, int start, int end, int profondeur){
 }
 
 int main(int argc, char** argv){
