c3cd845704509c1e0472fd6af588f6f28c1b182a
Marc-Olivier-Rancoeur <marc-olivier.rancoeur@isen-ouest.yncrea.fr>
2022-11-01 12:23:36 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void* ThdMergeSort(void* pointer){
+}
+
+int* MergeSort(int* tab, int start, int end){
+}
+
+int main(int argc, char** argv){
+    return 0;
+}
