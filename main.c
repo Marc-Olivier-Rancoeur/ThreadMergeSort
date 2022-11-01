2c749a990573230280ca5cfb961a0569bec5d973
Marc-Olivier-Rancoeur <marc-olivier.rancoeur@isen-ouest.yncrea.fr>
2022-11-01 15:49:38 -0400

    merge sort iter no thread, mutex init for thread number

--- main.c
+++ main.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdbool.h>
 
-#define THREADS 2
+#define THREADS 1
+
+pthread_mutex_t threadsDisponiblesMutex;
+int threadsDisponibles = THREADS-1;
 
 struct MergeStruct{
     int* tab;
     int start;
     int end;
-    int profondeur;
 };
 
-void* ThdMergeSort(void* pointer){
+void* ThdMergeSortIter(void* pointer){
 }
 
-int* MergeSort(int* tab, int start, int end, int profondeur){
+void MergeSortIter(int* tab, int start, int end){
+    if(end-start < 3){
+        if(tab[start] > tab[end]){
+            int val = tab[start];
+            tab[start] = tab[end];
+            tab[end] = val;
+        }
+    }else{
+        while(pthread_mutex_lock(&threadsDisponiblesMutex) != 0){}
+        if(threadsDisponibles > 0){
+            threadsDisponibles-=1;
+        }else{
+            MergeSortIter(tab, start, end/2);
+        }
+        MergeSortIter(tab, (end/2)+1, end);
+        pthread_mutex_unlock(&threadsDisponiblesMutex);
+    }
+}
+void MergeSort(int* tab, int size){
+    MergeSortIter(tab, 0, size-1);
 }
 
 int main(int argc, char** argv){
+    pthread_mutex_init(&threadsDisponiblesMutex, NULL);
     return 0;
 }
